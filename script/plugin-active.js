c50fb0758e44b3d77fde87ff7af50706609436bf
pushtiparikh <98299460+pushtiparikh@users.noreply.github.com>
2022-10-21 22:42:54 +0530

    Fixed bug with permission set and improved in code
    please check and update this function for your web

--- script/plugin-active.js
+++ script/plugin-active.js
@@ -4,15 +4,7 @@ $(document).ready(function() {
       $(this).toggleClass('active');
       $(".mobile-menu").toggleClass('visible');
   });
-  // Smoothscroll script
-  $('.nav-link').click(function() {
-      var dis = $(this),
-          disTarget = dis.data('target'),
-          ScrollTo = $(disTarget).offset().top;
-      dis.addClass('active').siblings('.nav-link').removeClass('active');
-      $('html,body').animate({ scrollTop: ScrollTo });
-  });
-  // contact form script
+   // contact form script
   $('.form-wrap input').blur(function() {
       tmpval = $(this).val();
       if (tmpval == '') {
@@ -23,6 +15,14 @@ $(document).ready(function() {
           $(this).removeClass('empty');
       }
   });
+  // Smoothscroll script
+  $('.nav-link').click(function() {
+      var dis = $(this),
+          disTarget = dis.data('target'),
+          ScrollTo = $(disTarget).offset().top;
+      dis.addClass('active').siblings('.nav-link').removeClass('active');
+      $('html,body').animate({ scrollTop: ScrollTo });
+  });
   // testimonial slider
   $('.testimonial-slider').bxSlider({
       auto: true,
@@ -30,8 +30,6 @@ $(document).ready(function() {
       infiniteLoop: true,
       controls: false
   });
-  // Changing the defaults 
-  window.sr = ScrollReveal();
   // Customizing a reveal set 
   sr.reveal('.each-service', { origin: 'bottom', distance: '100px', duration: 1000, delay: 0, easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)' });
   // sript for fixed header on scroll
